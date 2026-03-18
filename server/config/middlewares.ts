c8c90a220caac04d43287b4b862d9ba21b6afe3e
jeanrichardson610 <jean.richardson610@gmail.com>
2026-03-18 14:42:39 -0700

    testing

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -4,7 +4,14 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://fitness-tracker-lac-kappa.vercel.app'], // allow your frontend
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
